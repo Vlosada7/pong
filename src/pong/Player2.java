3ae9bcf4c0b0f47c52aa7c946d2ec9387ef4a18d
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-06-01 13:42:56 +0200

    Pong done

---
+++ src/pong/Player2.java
@@ -0,0 +1,46 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player2 {
+	
+	public boolean right,left;
+	
+	public int width, height;
+	
+	public int x,y;
+	public Player2(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width=30;
+		this.height=5;
+		
+	}
+	
+	
+	
+	public void tick() {
+		if(right) {
+			x++;
+		}
+		else if(left) {
+			x--;
+		}
+		//limite da tela//
+		if(x+width > Game.WIDTH) {
+			x= Game.WIDTH - width;
+		}
+		else if(x<0) {
+			x=0;
+		}
+		
+	
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.red);
+		g.fillRect((int)x,(int)y, width, height);
+	}
+
+}
